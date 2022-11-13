aa5a0c2f93d0db8cca0f12aeca1d537ce7004c1f
Prasiddha Khadka <be2018se630@gces.edu.np>
2022-11-13 15:54:42 +0545

    cart model

---
+++ lib/model/cart_model.dart
@@ -0,0 +1,47 @@
+class Cart {
+  late final int? id;
+  final String? productId;
+  final String? productName;
+  final String? initialPrice;
+  final String? productPrice;
+  final int? quantity;
+  final String? unitTag;
+  final String? image;
+
+  Cart(
+      {this.id,
+      this.productId,
+      this.productName,
+      this.initialPrice,
+      this.productPrice,
+      this.quantity,
+      this.unitTag,
+      this.image});
+
+  factory Cart.fromMap(Map<dynamic, dynamic> fromMap) {
+    return Cart(
+      id: fromMap['id'],
+      productId: fromMap['productId'],
+      productName: fromMap['prodcutName'],
+      initialPrice: fromMap['initialPrice'],
+      productPrice: fromMap['productPrice'],
+      quantity: fromMap['quantity'],
+      unitTag: fromMap['unitTag'],
+      image: fromMap['image'],
+    );
+  }
+
+  //
+  Map<dynamic, dynamic> toMap() {
+    return {
+      'id': id,
+      'productId': productId,
+      'productName': productName,
+      'initialPrice': initialPrice,
+      'productPrice': productPrice,
+      'quantity': quantity,
+      'unitTage': unitTag,
+      'image': image,
+    };
+  }
+}
